455c7ca679bdf31e54db7a05f49609e02961493c
Mehulkumar Parmar <m3hul.g4m3r@gmail.com>
2025-05-29 09:18:35 +0530

    setup astro blog, add monads-in-practice blog
    Signed-off-by: Mehulkumar Parmar <m3hul.g4m3r@gmail.com>

---
+++ src/lib/variables.ts
@@ -0,0 +1,49 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+	// Site metadata
+	name: "Mehulkumar",
+	username: "mkp7",
+	rootUrl: "https://mkp7.github.io/",
+	shortDescription: "thoughts and ideas",
+	longDescription: "",
+
+	// Social media links
+	githubProfile: "https://github.com/mkp7",
+	// twitterProfile: "https://twitter.com/",
+	linkedinProfile: "https://www.linkedin.com/in/m3hulkum4r",
+
+	// Common text names used throughout the site
+	articlesName: "Writings",
+	projectsName: "Projects",
+	viewAll: "View All",
+
+	// Common descriptions used throughout the site
+	noArticles: "No featured writings yet.",
+	noProjects: "No featured projects yet.",
+
+	// Blog metadata
+	blogTitle: "My Thoughts & Takes",
+	// blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+	blogShortDescription:
+		"Programming and software engineering thoughts and ideas.",
+	blogLongDescription:
+		"Programming and software engineering thoughts and ideas.",
+
+	// Project metadata
+	projectTitle: "Projects and Code",
+	projectShortDescription:
+		"A list of my web development projects and developer tools.",
+	projectLongDescription:
+		"All of my projects, including both frontend and full-stack applications.",
+
+	// Profile image
+	profileImage: "mehulkumar.jpeg",
+
+	// Menu items
+	menu: {
+		home: "/",
+		// projects: "/projects",
+		blog: "/blog",
+	},
+};
